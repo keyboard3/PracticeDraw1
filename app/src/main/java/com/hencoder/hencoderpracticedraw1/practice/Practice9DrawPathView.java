88ffb0dcc731db2d22ce0e4f2ecdbfe24a29782f
keyboard3 <keyboard3@icloud.com>
2017-09-12 17:15:53 +0800

    爱心花了不伦不类

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.java
@@ -2,6 +2,8 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -25,5 +27,16 @@ public class Practice9DrawPathView extends View {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawPath() 方法画心形
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setStyle(Paint.Style.FILL);
+        Path path = new Path();
+        path.setFillType(Path.FillType.WINDING);
+        path.addCircle(200f, 100f, 50, Path.Direction.CCW);
+        path.addCircle(300f, 100f, 50, Path.Direction.CCW);
+        path.moveTo(200, 100);
+        path.lineTo(250, 250);
+        path.lineTo(300, 100);
+        path.close();
+        canvas.drawPath(path, paint);
     }
 }
