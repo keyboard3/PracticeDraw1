cd694595ece9ec7b5c7a16ead631f243543eff67
keyboard3 <keyboard3@icloud.com>
2017-09-12 22:27:01 +0800

    稍微修改爱心图

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.os.Build;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -22,20 +24,25 @@ public class Practice9DrawPathView extends View {
         super(context, attrs, defStyleAttr);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawPath() 方法画心形
+        int lx = 200;
+        int ly = 100;
+        int rx = 400;
+        int ry = 100;
         Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         paint.setStyle(Paint.Style.FILL);
         Path path = new Path();
         path.setFillType(Path.FillType.WINDING);
-        path.addCircle(200f, 100f, 50, Path.Direction.CCW);
-        path.addCircle(300f, 100f, 50, Path.Direction.CCW);
-        path.moveTo(200, 100);
-        path.lineTo(250, 250);
-        path.lineTo(300, 100);
+        path.addArc(lx, ly - 50, lx + 100, ly + 50, 0, -180);
+        path.addArc(rx - 100, ry - 50, rx, ly + 50, 0, -180);
+        path.moveTo(lx, ly);
+        path.lineTo((lx + rx) / 2, ly + 100);
+        path.lineTo(rx, ry);
         path.close();
         canvas.drawPath(path, paint);
     }
